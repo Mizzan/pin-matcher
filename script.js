6ae743517e85cebb399270db89ef69aa06afa597
Mizanur Rahman <mizzanur007@gmail.com>
2021-01-31 14:17:47 +0600

    ✔ Modified CSS and script file

---
+++ script.js
@@ -0,0 +1,12 @@
+// method 1
+// function randomPin() {
+//   return Math.round((Math.random() * 10000).toFixed(4));
+// }
+// var getLength = (Math.log(randomPin()) * Math.LOG10E + 1) | 0;
+// let fourDigitNumber;
+
+// getLength === 4 ? (fourDigitNumber = randomPin()) : randomPin();
+
+// console.log(getLength)
+
+// Method 2
